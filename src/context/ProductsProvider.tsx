b32683de2c973313dcfcfea4e7916c430a0ece5f
rejordon93 <rejordon17@gmail.com>
2024-07-19 20:18:55 -0700

    Adding ProductsProviderContext

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,5 +1,44 @@
-import React from "react";
+import { createContext, ReactElement, useEffect, useState } from "react";
 
-export const ProductsProvider = () => {
-  return <div>ProductsProvider</div>;
+export type ProductType = {
+  sku: string;
+  name: string;
+  price: number;
 };
+
+const initState: ProductType[] = [];
+
+export type UseProductsContextState = { products: ProductType[] };
+
+const initContextState: UseProductsContextState = { products: initState };
+
+const ProductsContext =
+  createContext<UseProductsContextState>(initContextState);
+
+type ChildrenType = { children?: ReactElement | ReactElement[] };
+
+export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
+  const [products, setProducts] = useState<ProductType[]>(initState);
+
+  useEffect(() => {
+    const fetchProducts = async (): Promise<ProductType[]> => {
+      const data = await fetch("http://localhost:3500/products")
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => {
+          if (err instanceof Error) console.log(err.message);
+        });
+      return data;
+    };
+    fetchProducts().then((products) => setProducts(products));
+  }, []);
+
+  return (
+    <ProductsContext.Provider value={{ products }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+
+export default ProductsContext;
